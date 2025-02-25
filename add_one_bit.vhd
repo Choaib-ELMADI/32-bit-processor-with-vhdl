57f581fa6f011950bdf7eb6e3adce0ed74dd3909
Choaib ELMADI <choaibamd@gmail.com>
2025-02-25 21:56:35 +0000

    Refactor the one bit adder module

---
+++ add_one_bit.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity add_one_bit is
+    port (
+        a, b, c_in : in  std_logic;
+        s, c_out   : out std_logic
+    );
+end add_one_bit;
+
+architecture add_one_bit_arch of add_one_bit is
+    begin
+        s     <= (a xor b) xor c_in;
+        c_out <= (a and b) or (c_in and (a xor b));
+    end
+end add_one_bit_arch;
